d5c3af801afb8ae2659c385a11eb3a085c6c0d55
emi <kairosart2007@gmail.com>
2018-10-22 21:55:54 +0200

    Final commit

--- qlearning_robot/QLearner.py
+++ qlearning_robot/QLearner.py
@@ -1,40 +1,16 @@
-"""  		   	  			    		  		  		    	 		 		   		 		  
-Template for implementing QLearner  (c) 2015 Tucker Balch  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		   	  			    		  		  		    	 		 		   		 		  
-Atlanta, Georgia 30332  		   	  			    		  		  		    	 		 		   		 		  
-All Rights Reserved  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Template code for CS 4646/7646  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Georgia Tech asserts copyright ownership of this template and all derivative  		   	  			    		  		  		    	 		 		   		 		  
-works, including solutions to the projects assigned in this course. Students  		   	  			    		  		  		    	 		 		   		 		  
-and other users of this template code are advised not to share it with others  		   	  			    		  		  		    	 		 		   		 		  
-or to make it available on publicly viewable websites including repositories  		   	  			    		  		  		    	 		 		   		 		  
-such as github and gitlab.  This copyright statement should not be removed  		   	  			    		  		  		    	 		 		   		 		  
-or edited.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-We do grant permission to share solutions privately with non-students such  		   	  			    		  		  		    	 		 		   		 		  
-as potential employers. However, sharing with other current or future  		   	  			    		  		  		    	 		 		   		 		  
-students of CS 7646 is prohibited and subject to being investigated as a  		   	  			    		  		  		    	 		 		   		 		  
-GT honor code violation.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
------do not edit anything above this line---  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Student Name: Tucker Balch (replace with your name)  		   	  			    		  		  		    	 		 		   		 		  
-GT User ID: tb34 (replace with your User ID)  		   	  			    		  		  		    	 		 		   		 		  
-GT ID: 900897987 (replace with your GT ID)  		   	  			    		  		  		    	 		 		   		 		  
-"""  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-import numpy as np  		   	  			    		  		  		    	 		 		   		 		  
-import random as rand  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-class QLearner(object):  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
+"""Implement QLearner, a Reinforcement Learning class"""
+
+import numpy as np
+import random as rand
+from copy import deepcopy
+
+class QLearner(object):
+
     def __init__(self, num_states=100, num_actions=4, alpha=0.2,
         gamma=0.9, rar=0.5, radr=0.99, dyna=0, verbose=False):
         """The constructor QLearner() reserves space for keeping track of Q[s, a] for 
         the number of states and actions. It initializes Q[] with all zeros.
+
         Parameters:
         num_states: int, the number of states to consider
         num_actions: int, the number of actions available
@@ -78,6 +54,7 @@ class QLearner(object):
         """Find the next action to take in state s. Update the latest state and action 
         without updating the Q table. Two main uses for this method: 1) To set the  
         initial state, and 2) when using a learned policy, but not updating it.
+
         Parameters:
         s: The new state
         
@@ -97,6 +74,7 @@ class QLearner(object):
         """Find the next action to take in state s_prime. Update the latest state 
         and action and the Q table. Update rule:
         Q'[s, a] = (1 - α) · Q[s, a] + α · (r + γ · Q[s', argmax a'(Q[s', a'])]).
+
         Parameters:
         s_prime: int, the new state
         r: float, a real valued immediate reward for taking the previous action
@@ -142,4 +120,3 @@ class QLearner(object):
         if self.verbose: 
             print ("s =", s_prime,"a =",a_prime,"r =",r)
         return a_prime
-	   	  			    		  		  		    	 		 		   		 		  
