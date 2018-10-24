9391e9f242a16b3e2de3c9e48b75cf858e009ff3
emi <kairosart2007@gmail.com>
2018-10-24 20:52:07 +0200

    Final commit

---
+++ strategy_learner/.ipynb_checkpoints/marketsim-checkpoint.py
@@ -0,0 +1,289 @@
+"""Market simulator that processes a dataframe instead of a csv file"""
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+#import matplotlib.pyplot as plt
+from analysis import get_portfolio_value, get_portfolio_stats, plot_normalized_data
+from util import get_data, normalize_data
+
+# Add plotly for interactive charts
+from plotly.offline import init_notebook_mode, iplot
+init_notebook_mode(connected=True)
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly import tools
+
+def compute_portvals(df_orders, start_val=1000000, commission=9.95, impact=0.005):
+    """
+    Parameters:
+    df_orders: A dataframe with orders for buying or selling stocks
+    start_val: The starting value of the portfolio (initial cash available)
+    commission: The fixed amount in dollars charged for each transaction (both entry and exit)
+    impact: The amount the price moves against the trader compared to the historical data at each transaction
+    
+    Returns:
+    portvals: A dataframe with one column containing the value of the portfolio for each trading day
+    """
+
+    # Sort the orders dataframe by date
+    df_orders.sort_index(ascending=True, inplace=True)
+    
+    # Get the start and end dates and symbols
+    start_date = df_orders.index.min()
+    end_date = df_orders.index.max()
+    symbols = df_orders.Symbol.unique().tolist()
+
+    # Create a dataframe with adjusted close prices for the symbols and for cash
+    df_prices = get_data(symbols, pd.date_range(start_date, end_date), addSPY=True)
+    del df_prices["SPY"]
+    df_prices["cash"] = 1.0
+
+    # Fill NAN values if any
+    df_prices.fillna(method="ffill", inplace=True)
+    df_prices.fillna(method="bfill", inplace=True)
+    df_prices.fillna(1.0, inplace=True)
+
+    # Create a dataframe that represents changes in the number of shares by day for each asset. 
+    # It has the same structure as df_prices, and is initially filled with zeros
+    df_trades = pd.DataFrame(np.zeros((df_prices.shape)), df_prices.index, df_prices.columns)
+    for index, row in df_orders.iterrows():
+        # Total value of shares purchased or sold
+        traded_share_value = df_prices.loc[index, row["Symbol"]] * row["Shares"]
+        # Transaction cost 
+        transaction_cost = commission + impact * traded_share_value
+
+        # Update the number of shares and cash based on the type of transaction done
+        # Note: The same asset may be traded more than once on a particular day
+        if row["Order"] == "BUY":
+            df_trades.loc[index, row["Symbol"]] = df_trades.loc[index, row["Symbol"]] + row["Shares"]
+            df_trades.loc[index, "cash"] = df_trades.loc[index, "cash"] + traded_share_value * (-1.0) - transaction_cost
+        else:
+            df_trades.loc[index, row["Symbol"]] = df_trades.loc[index, row["Symbol"]] -row["Shares"]
+            df_trades.loc[index, "cash"] = df_trades.loc[index, "cash"] + traded_share_value - transaction_cost
+
+    # Create a dataframe that represents on each particular day how much of each asset in the portfolio
+    # It has the same structure as df_prices, and is initially filled with zeros
+    df_holdings = pd.DataFrame(np.zeros((df_prices.shape)), df_prices.index, df_prices.columns)
+    for row_count in range(len(df_holdings)):
+        # In the first row, the shares are the same as in df_trades, but start_val must be added to cash
+        if row_count == 0:
+            df_holdings.iloc[0, :-1] = df_trades.iloc[0, :-1].copy()
+            df_holdings.iloc[0, -1] = df_trades.iloc[0, -1] + start_val
+        # The rest of the rows show cumulative values
+        else:
+            df_holdings.iloc[row_count] = df_holdings.iloc[row_count-1] + df_trades.iloc[row_count]
+        row_count += 1
+
+    # Create a dataframe that represents the monetary value of each asset in the portfolio
+    df_value = df_prices * df_holdings
+    
+    # Create portvals dataframe
+    portvals = pd.DataFrame(df_value.sum(axis=1), df_value.index, ["port_val"])
+    return portvals
+
+
+def market_simulator(df_orders, df_orders_benchmark, start_val=1000000, commission=9.95, 
+    impact=0.005, daily_rf=0.0, samples_per_year=252.0, vertical_lines=False):
+    """
+    This function takes in and executes trades from orders dataframes
+    Parameters:
+    df_orders: A dataframe that contains portfolio orders
+    df_orders_benchmark: A dataframe that contains benchmark orders
+    start_val: The starting cash in dollars
+    commission: The fixed amount in dollars charged for each transaction (both entry and exit)
+    impact: The amount the price moves against the trader compared to the historical data at each transaction
+    daily_rf: Daily risk-free rate, assuming it does not change
+    samples_per_year: Sampling frequency per year
+    vertical_lines: Showing vertical lines for buy and sell orders
+    
+    Returns:
+    Print out final portfolio value of the portfolio, as well as Sharpe ratio, 
+    cumulative return, average daily return and standard deviation of the portfolio and Benchmark.
+    Plot a chart of the portfolio and benchmark performances
+    """
+    
+    # Process portfolio orders
+    portvals = compute_portvals(df_orders=df_orders, start_val=start_val, 
+        commission=commission, impact=impact)
+
+    # Get portfolio stats
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals,
+     daily_rf=daily_rf, samples_per_year=samples_per_year)
+    
+    # Process benchmark orders
+    portvals_bm = compute_portvals(df_orders=df_orders_benchmark, start_val=start_val, 
+        commission=commission, impact=impact)
+    
+    # Get benchmark stats
+    cum_ret_bm, avg_daily_ret_bm, std_daily_ret_bm, sharpe_ratio_bm = get_portfolio_stats(portvals_bm,
+     daily_rf=daily_rf, samples_per_year=samples_per_year)
+
+    # Get Final values
+    final_value = portvals.iloc[-1, -1]
+    final_value_bm = portvals_bm.iloc[-1, -1]
+    
+    # Get orders number
+    orders_count = len(portvals.index)
+    
+    # Compare portfolio against Benchmark
+    print ("Sharpe Ratio of Portfolio: {}".format(sharpe_ratio))
+    print ("Sharpe Ratio of Benchmark : {}".format(sharpe_ratio_bm))
+    print ()
+    print ("Cumulative Return of Portfolio: {}".format(cum_ret))
+    print ("Cumulative Return of Benchmark : {}".format(cum_ret_bm))
+    print ()
+    print ("Standard Deviation of Portfolio: {}".format(std_daily_ret))
+    print ("Standard Deviation of Benchmark : {}".format(std_daily_ret_bm))
+    print ()
+    print ("Average Daily Return of Portfolio: {}".format(avg_daily_ret))
+    print ("Average Daily Return of Benchmark : {}".format(avg_daily_ret_bm))
+    print ()
+    print ("Final Portfolio Value: {}".format(portvals.iloc[-1, -1]))
+    print ("Final Benchmark Value: {}".format(portvals_bm.iloc[-1, -1]))
+    print ()
+    print ("Portfolio Orders count: {}".format(len(portvals.index)))
+    
+
+    # Rename columns and normalize data to the first date of the date range
+    portvals.rename(columns={"port_val": "Portfolio"}, inplace=True)
+    portvals_bm.rename(columns={"port_val": "Benchmark"}, inplace=True)
+    
+    plot_norm_data_vertical_lines(df_orders, portvals, portvals_bm, vertical_lines)
+    
+    return orders_count, sharpe_ratio, cum_ret, std_daily_ret, avg_daily_ret, final_value
+
+def plot_norm_data_vertical_lines(df_orders, portvals, portvals_bm, vert_lines=False):
+    """Plots portvals and portvals_bm, showing vertical lines for buy and sell orders
+    
+    Parameters:
+    df_orders: A dataframe that contains portfolio orders
+    portvals: A dataframe with one column containing daily portfolio value
+    portvals_bm: A dataframe with one column containing daily benchmark value
+    save_fig: Whether to save the plot or not
+    fig_name: The name of the saved figure
+
+    Returns: Plot a chart of the portfolio and benchmark performances
+    """
+    # Normalize data
+    portvals = normalize_data(portvals)
+    portvals_bm = normalize_data(portvals_bm)
+    df = portvals_bm.join(portvals)
+  
+    # Min range
+    if (df.loc[:, "Benchmark"].min() < df.loc[:, "Portfolio"].min()):
+        min_range = df.loc[:, "Benchmark"].min()
+    else:
+        min_range = df.loc[:, "Portfolio"].min()
+        
+    # Max range    
+    if (df.loc[:, "Benchmark"].max() > df.loc[:, "Portfolio"].max()):
+        max_range = df.loc[:, "Benchmark"].max()
+    else:
+        max_range = df.loc[:, "Portfolio"].max()  
+        
+    # Plot the normalized benchmark and portfolio        
+    trace_bench = go.Scatter(
+                x=df.index,
+                y=df.loc[:, "Benchmark"],
+                name = "Benchmark",
+                line = dict(color = '#17BECF'),
+                opacity = 0.8)
+
+    trace_porfolio = go.Scatter(
+                x=df.index,
+                y=df.loc[:, "Portfolio"],
+                name = "Portfolio",
+                line = dict(color = '#000000'),
+                opacity = 0.8)
+
+    data = [trace_bench, trace_porfolio]
+    
+    
+
+    
+    # Plot the vertical lines for buy and sell signals
+    shapes = list()
+    if vert_lines:
+        buy_line = []
+        sell_line = []
+        for date in df_orders.index:
+            if df_orders.loc[date, "Order"] == "BUY":
+                buy_line.append(date)
+            else:
+                sell_line.append(date)
+        # Vertical lines
+        line_size = max_range + (max_range * 10 / 100)
+
+        # Buy line
+        for i in buy_line:
+            shapes.append({'type': 'line',
+                           'xref': 'x',
+                           'yref': 'y',
+                           'x0': i,
+                           'y0': 0,
+                           'x1': i,
+                           'y1': line_size,
+                           'line': {
+                                'color': 'rgb(0, 102, 34)',
+                                'width': 1,
+                                'dash': 'dash',
+                            },
+                          })
+        # Sell line
+        for i in sell_line:
+            shapes.append({'type': 'line',
+                           'xref': 'x',
+                           'yref': 'y',
+                           'x0': i,
+                           'y0': 0,
+                           'x1': i,
+                           'y1': line_size,
+                           'line': {
+                                'color': 'rgb(255, 0, 0)',
+                                'width': 1,
+                                'dash': 'dash',
+                            },
+                          })
+    
+    layout = dict(
+        autosize=True,
+        shapes=shapes,
+        margin=go.Margin(
+            l=50,
+            r=50,
+            b=100,
+            t=100,
+            pad=4
+        ),
+        title = "Portfolio vs Benchmark",
+        xaxis = dict(
+                title='Dates',
+                rangeselector=dict(
+                    buttons=list([
+                        dict(count=1,
+                            label='1m',
+                            step='month',
+                            stepmode='backward'),
+                        dict(count=6,
+                            label='6m',
+                            step='month',
+                            stepmode='backward'),
+                        dict(step='all')
+                    ])
+                ),
+                range = [portvals.index[0], portvals.index[-1]]),
+            
+        yaxis = dict(
+                title='Normalized Prices',
+                range = [min_range - (min_range * 10 / 100) ,max_range + (max_range * 10 / 100)]),
+                    
+        )
+        
+    
+        
+        
+
+    fig = dict(data=data, layout=layout)
+    iplot(fig)
+    
